b43ae28f3cf7472cd4d16ecdfde46fd327f6c751
Rob Mosher <rob@robmosher.com>
2024-09-08 16:17:09 -0400

    Add custom 404, update readme

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 # TODOs
 
 - Ensure `italic` utility class can change font to italics
+- CloudFront 404 errors when visiting non-root paths directly
---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import { Heading, Section } from '@components'
+
+const NotFound = () => (
+  <Section className='text-center'>
+    <>
+      <Heading as='h1'>404 - Page Not Found</Heading>
+      <p>The page you are looking for does not exist.</p>
+    </>
+  </Section>
+)
+
+export default NotFound
