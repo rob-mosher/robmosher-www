6dac5fe1b96896d0a4100f42892c06fdb4d0899d
Rob Mosher <rob@robmosher.com>
2024-09-08 07:52:21 -0400

    Fix: Remove ID from Footer+Header, add type check

--- src/components/Footer/data.tsx
+++ src/components/Footer/data.tsx
@@ -5,25 +5,21 @@ import type { TSocialMedia } from '../../types/socialMedia'
 
 export const socialMediaItems: TSocialMedia[] = [
   {
-    id: 'social-media-item-youtube',
     ariaLabel: 'YouTube Channel',
     href: 'https://www.youtube.com/user/robmoshermusic/',
     logo: <YouTubeIcon />,
   },
   {
-    id: 'social-media-item-facebook',
     ariaLabel: 'Facebook Page',
     href: 'https://facebook.com/robmosher',
     logo: <FacebookIcon />,
   },
   {
-    id: 'social-media-item-github',
     ariaLabel: 'GitHub Profile',
     href: 'https://github.com/rob-mosher',
     logo: <GitHubIcon />,
   },
   {
-    id: 'social-media-item-linkedin',
     ariaLabel: 'LinkedIn Profile',
     href: 'http://linkedin.com/in/rob-mosher-it',
     logo: <LinkedInIcon />,
--- src/components/Header/data.ts
+++ src/components/Header/data.ts
@@ -2,22 +2,18 @@ import type { TNav } from '@types'
 
 export const navItems: TNav[] = [
   {
-    id: 'navitem-new',
     href: '/new',
     title: 'New',
   },
   {
-    id: 'navitem-projects',
     href: '/',
     title: 'Projects',
   },
   {
-    id: 'navitem-videos',
     href: '/videos',
     title: 'Videos',
   },
   {
-    id: 'navitem-contact',
     href: '/contact',
     title: 'Contact',
   },
