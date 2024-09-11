396c2f90e4b379d686e3ba6462b7a863667e3598
Rob Mosher <rob@robmosher.com>
2024-09-11 08:44:18 -0400

    Several UI Tweaks
    - Add shadow to homepage cta button
    
    - Add margin to About text in responsive layout
    
    - Change dash to emdash in title delimiter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ const Home = () => {
         <div className='absolute inset-0 z-10 bg-gradient-radial from-black/15 via-black/20 to-black/50' />
         <div className='z-50 flex h-full flex-col items-start justify-end gap-9'>
           <p className='z-50 flex text-2xl text-white'>Composer, Woodwinds, & Technology</p>
-          <Button href='/new'>
+          <Button href='/new' className='shadow-2xl'>
             New Releases
           </Button>
         </div>
@@ -35,7 +35,7 @@ const Home = () => {
       <Section outerClassName='bg-white' className='py-12'>
         <div className='flex flex-col md:flex-row'>
           <Heading className='flex-[40%]' as='h1'>About</Heading>
-          <p className='flex-[60%] leading-[1.85] text-gray-700'>
+          <p className='mt-4 flex-[60%] leading-[1.85] text-gray-700 md:mt-0'>
             These projects represent the care, craft, and ridiculousness I love to bring to everything I do. I believe that inspiration, coupled with dedication and freedom from ego, lead to an endless, inevitable reshuffling, shaped by the beauty of limitation. The exploration of Science and Art is a privilege and duty that we collectively explore and share together.
           </p>
         </div>
--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -2,4 +2,4 @@ export const defaultPageTitle: string = 'Rob Mosher'
 export const defaultPageTitleHomepage: string = 'Rob Mosher - Composer, Woodwinds, & Technology'
 export const defaultMetaDescription: string = 'Official site for Rob Mosher, featuring music, projects, and more'
 
-export const getPageTitle = (title: string) => `${title} - ${defaultPageTitle}`
+export const getPageTitle = (title: string) => `${title} — ${defaultPageTitle}`
