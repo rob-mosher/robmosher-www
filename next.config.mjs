e78c4750c78f9db2a447f3ce60a22e83086cd35a
Rob Mosher <rob@robmosher.com>
2024-09-11 12:49:24 -0400

    Remove SSG export override
    - Utilize Vercel's platform now that site is no longer on CloudFront

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,12 @@ if (!process.env.NEXT_PUBLIC_CDN_HOST) {
   throw new Error('Missing environment variable: NEXT_PUBLIC_CDN_HOST')
 }
 
+// Uncomment the below to support SSG export
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
+  // output: 'export',
+  // images: {
+  //   unoptimized: true,
+  // },
   env: {
     NEXT_PUBLIC_CDN_HOST: process.env.NEXT_PUBLIC_CDN_HOST, // Make available client-side
   },
