4e32610295c08301a7ccca3622d4150e879f6feb
Rob Mosher <rob@robmosher.com>
2025-06-30 09:06:52 -0400

    fix: update text order in Home component for improved clarity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ const Home = () => (
   <Section outerClassName='relative bg-robMosher bg-center bg-cover h-[75vh] shadow-inner'>
     <div className='absolute inset-0 z-10 bg-radialGradient from-black/15 via-black/20 to-black/50' />
     <div className='z-50 flex h-full flex-col items-start justify-end gap-4 md:gap-9'>
-      <p className='z-50 flex text-1.5xl text-white md:text-2xl'>Composer, Woodwinds, Technology, & Advocate</p>
+      <p className='z-50 flex text-1.5xl text-white md:text-2xl'>Composer, Technology, Woodwinds, & Advocate</p>
       <div className='flex flex-col gap-4 sm:flex-row'>
         <Button href='/new' className='shadow-2xl'>
           New
