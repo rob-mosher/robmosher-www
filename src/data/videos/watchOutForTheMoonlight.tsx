8107e891be028e847d7f89af3919e7b305e66fbc
Rob Mosher <rob@robmosher.com>
2025-07-14 21:51:19 -0400

    feat: add lyrics section to "Watch Out for the Moonlight" video

--- src/data/videos/watchOutForTheMoonlight.tsx
+++ src/data/videos/watchOutForTheMoonlight.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react'
+import { Heading } from '@components'
 import type { TVideo, TBlogItem } from '@types'
 
 const descriptionJSX: ReactNode = (
@@ -11,6 +12,27 @@ const descriptionJSX: ReactNode = (
     </p>
     <p className='mt-4 italic'>
       Image created by DALL-E from OpenAI.
+    </p>
+    <Heading as='h4' className='mt-6'>
+      Lyrics
+    </Heading>
+    <p>
+      Watch out for the moonlight,
+      <br />
+      It&apos;s coming for you.
+      <br />
+      Watch out for the moonlight,
+      <br />
+      Oooo.
+      <br />
+      Watch out for the moonlight,
+      <br />
+      It&apos;s coming for you.
+      <br />
+      <br />
+      Here comes the sunrise,
+      <br />
+      It&apos;s warm and filling the sky!
     </p>
   </>
 )
